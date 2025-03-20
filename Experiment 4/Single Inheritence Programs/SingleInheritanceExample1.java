4ff0109e2fb55a95510149de5e5bd74e70e96a22
Nagarampalli Sarvan Kumar <sarvankumarnagarampalli478@gmail.com>
2025-03-20 18:11:20 +0530

    Add files via upload

---
+++ Experiment 4/Single Inheritence Programs/SingleInheritanceExample1.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+class Book {
+    String title;
+
+    void getTitle(Scanner sc) {
+        System.out.print("Enter book title: ");
+        title = sc.nextLine();
+    }
+
+    void displayTitle() {
+        System.out.println("Book Title: " + title);
+    }
+}
+
+class Novel extends Book {
+    String author;
+
+    void getAuthor(Scanner sc) {
+        System.out.print("Enter author name: ");
+        author = sc.nextLine();
+    }
+
+    void displayAuthor() {
+        System.out.println("Author: " + author);
+    }
+}
+
+public class SingleInheritanceExample1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Novel novel = new Novel();
+        novel.getTitle(sc);
+        novel.getAuthor(sc);
+        novel.displayTitle();
+        novel.displayAuthor();
+
+        sc.close();
+    }
+}
