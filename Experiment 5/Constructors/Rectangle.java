a11cc9ba7deda979f1b614c837f811e2a7a9a613
Nagarampalli Sarvan Kumar <sarvankumarnagarampalli478@gmail.com>
2025-03-24 11:39:25 +0530

    Update Rectangle.java

--- Experiment 5/Constructors/Rectangle.java
+++ Experiment 5/Constructors/Rectangle.java
@@ -3,13 +3,11 @@ import java.util.Scanner;
 class Rectangle {
     double length, width;
 
-    // Constructor
     Rectangle(double length, double width) {
         this.length = length;
         this.width = width;
     }
 
-    // Method to calculate area
     double calculateArea() {
         return length * width;
     }
